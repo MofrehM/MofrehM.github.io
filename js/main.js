ee46b34012f2d577fc873b8fa934a5b6cc7f6108
Mohammed Mofreh <98338013+MofrehM@users.noreply.github.com>
2024-03-19 17:05:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let minutesVar = document.querySelector(".calender-minutes")
 let secondsVar = document.querySelector(".calender-seconds")
 
 window.onload = function(){
-    let eventDate = new Date("8 Nov 2022 19:40:00").getTime();
+    let eventDate = new Date("8 Nov 2024 19:40:00").getTime();
    let eventInterval = setInterval(() => {
         let nowDate = new Date().getTime();
         let totalDate = eventDate - nowDate;
